6650d123685e287fdb00447727bb654017b1a55f
italiacardenas <cardenasitalia@gmail.com>
2021-06-28 00:07:03 -0700

    deliverables

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -4,3 +4,12 @@ Mecha_Car_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors 
 View(Mecha_Car_table)
 mecha_car_lm <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=Mecha_Car_table) #generate multiple linear regression model
 summary(mecha_car_lm) #generate summary
+
+
+#deliverable 2
+Suspension_Coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
+# total_summary
+total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create total summary table 
+
+# lot_summary
+lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create lot summary table
