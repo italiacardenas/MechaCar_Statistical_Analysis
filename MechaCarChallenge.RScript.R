1c673ce8a448831498edb79ef04311b1d34e2344
italiacardenas <cardenasitalia@gmail.com>
2021-06-28 00:47:16 -0700

    deliverablesz

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -10,6 +10,16 @@ summary(mecha_car_lm) #generate summary
 Suspension_Coil_table <- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F)
 # total_summary
 total_summary <- Suspension_Coil_table %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create total summary table 
-
 # lot_summary
 lot_summary <- Suspension_Coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI), .groups = 'keep') #create lot summary table
+
+
+#deliverable 3
+#t-test
+t.test(Suspension_Coil_table$PSI, mu=1500)
+#t-test on lot1
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+#t-test on lot2
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+#t-test on lot3
+t.test(subset(Suspension_Coil_table,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
